5fa35fdcd3ed710b396bc5aa7a20462f83c877f5
charlesgrisar <charles.grisar@gmail.com>
2015-05-14 19:25:12 +0200

    semantic editor v 0.1 done

---
+++ app/Http/Controllers/TmpAddressesController.php
@@ -0,0 +1,90 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Movingwine\Datatable;
+use App\Movingwine\TmpAddress;
+use Illuminate\Http\Request;
+
+class TmpAddressesController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+		$addresses = Datatable::output(TmpAddress::all(), ['key'=> 'id', 'newkey' => 'Dt_Rowid', 'transform' => 'row_']);
+		return ['draw' => '1', 'RecordCount' => $addresses->count(), 'data' => $addresses];
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store(Requests\AddressRequest $request)
+	{
+		//
+		TmpAddress::create($request->all());
+		return '{"success": true}';
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
